81e3064196cf329edaf06aa98c11e4924d28b400
Christian Balaguer Jehle <christian.balaguer@namics.com>
2012-06-13 12:32:44 +0200

    rename GridViewConsole to ConsoleGridView

---
+++ game-of-life/src/main/ch/crisi/tdd/gameOfLife/ConsoleGridView.java
@@ -0,0 +1,22 @@
+package ch.crisi.tdd.gameOfLife;
+
+public class ConsoleGridView extends AbstractGridView {
+
+    @Override
+    public void render() {
+        for (int x = 0; x < grid.getSize(); x++) {
+            for (int y = 0; y < grid.getSize(); y++) {
+                if(grid.isAlive(x,y)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("-");
+                }
+
+                if(y == grid.getSize() - 1) {
+                    System.out.print("\n");
+                }
+            }
+        }
+    }
+
+}
