3da84a8cccfea1c823e460fd40d7cdd1990eba8d
Christian Balaguer Jehle <christian.balaguer@namics.com>
2012-06-27 13:59:16 +0200

    add static import for methods

--- percent-encoder/src/test/ch/crisi/tdd/percentEncoder/URLPercentEncoderTest.java
+++ percent-encoder/src/test/ch/crisi/tdd/percentEncoder/URLPercentEncoderTest.java
@@ -2,140 +2,120 @@ package ch.crisi.tdd.percentEncoder;
 
 import org.junit.Test;
 
+import static ch.crisi.tdd.percentEncoder.URLPercentEncoder.encode;
+import static ch.crisi.tdd.percentEncoder.URLPercentEncoder.encodeAll;
 import static junit.framework.Assert.assertEquals;
 
 public class URLPercentEncoderTest {
 
     @Test
     public void encodeExclamationMark() {
-        String actual = URLPercentEncoder.encode("!");
-        assertEquals("%21", actual);
+        assertEquals("%21", encode("!"));
     }
 
     @Test
     public void encodeNumberSign() {
-        String actual = URLPercentEncoder.encode("#");
-        assertEquals("%23", actual);
+        assertEquals("%23", encode("#"));
     }
 
     @Test
     public void encodeDollarSign() {
-        String actual = URLPercentEncoder.encode("$");
-        assertEquals("%24", actual);
+        assertEquals("%24", encode("$"));
     }
 
     @Test
     public void encodePercentSign() {
-        String actual = URLPercentEncoder.encode("%");
-        assertEquals("%25", actual);
+        assertEquals("%25", encode("%"));
     }
 
     @Test
     public void encodeAmpersand() {
-        String actual = URLPercentEncoder.encode("&");
-        assertEquals("%26", actual);
+        assertEquals("%26", encode("&"));
     }
 
     @Test
     public void encodeApostrophe() {
-        String actual = URLPercentEncoder.encode("'");
-        assertEquals("%27", actual);
+        assertEquals("%27", encode("'"));
     }
 
     @Test
     public void encodeOpeningBracket() {
-        String actual = URLPercentEncoder.encode("(");
-        assertEquals("%28", actual);
+        assertEquals("%28", encode("("));
     }
 
     @Test
     public void encodeClosingBracket() {
-        String actual = URLPercentEncoder.encode(")");
-        assertEquals("%29", actual);
+        assertEquals("%29", encode(")"));
     }
 
     @Test
     public void encodeAsterisk() {
-        String actual = URLPercentEncoder.encode("*");
-        assertEquals("%2A", actual);
+        assertEquals("%2A", encode("*"));
     }
 
     @Test
     public void encodePlusSign() {
-        String actual = URLPercentEncoder.encode("+");
-        assertEquals("%2B", actual);
+        assertEquals("%2B", encode("+"));
     }
 
     @Test
     public void encodeComma() {
-        String actual = URLPercentEncoder.encode(",");
-        assertEquals("%2C", actual);
+        assertEquals("%2C", encode(","));
     }
 
     @Test
     public void encodeSlash() {
-        String actual = URLPercentEncoder.encode("/");
-        assertEquals("%2F", actual);
+        assertEquals("%2F", encode("/"));
     }
 
     @Test
     public void encodeColon() {
-        String actual = URLPercentEncoder.encode(":");
-        assertEquals("%3A", actual);
+        assertEquals("%3A", encode(":"));
     }
 
     @Test
     public void encodeSemicolon() {
-        String actual = URLPercentEncoder.encode(";");
-        assertEquals("%3B", actual);
+        assertEquals("%3B", encode(";"));
     }
 
     @Test
     public void encodeEqualSign() {
-        String actual = URLPercentEncoder.encode("=");
-        assertEquals("%3D", actual);
+        assertEquals("%3D", encode("="));
     }
 
     @Test
     public void encodeQuestionMark() {
-        String actual = URLPercentEncoder.encode("?");
-        assertEquals("%3F", actual);
+        assertEquals("%3F", encode("?"));
     }
 
     @Test
     public void encodeAt() {
-        String actual = URLPercentEncoder.encode("@");
-        assertEquals("%40", actual);
+        assertEquals("%40", encode("@"));
     }
 
     @Test
     public void encodeOpeningSquareBracket() {
-        String actual = URLPercentEncoder.encode("[");
-        assertEquals("%5B", actual);
+        assertEquals("%5B", encode("["));
     }
 
     @Test
     public void encodeClosingSquareBracket() {
-        String actual = URLPercentEncoder.encode("]");
-        assertEquals("%5D", actual);
+        assertEquals("%5D", encode("]"));
     }
 
     @Test
     public void encodeEmptySpace() {
-        String actual = URLPercentEncoder.encode(" ");
-        assertEquals("%20", actual);
+        assertEquals("%20", encode(" "));
     }
 
     @Test
     public void encodeMultipleReservedAndUnreservedChars() {
-        String actual = URLPercentEncoder.encode("http://www.google.com");
-        assertEquals("http%3A%2F%2Fwww.google.com", actual);
+        assertEquals("http%3A%2F%2Fwww.google.com", encode("http://www.google.com"));
     }
 
     @Test
     public void encodeAllChars() {
-        String actual = URLPercentEncoder.encodeAll("http://www.google.com");
-        assertEquals("%68%74%74%70%3A%2F%2F%77%77%77%2E%67%6F%6F%67%6C%65%2E%63%6F%6D", actual);
+        assertEquals("%68%74%74%70%3A%2F%2F%77%77%77%2E%67%6F%6F%67%6C%65%2E%63%6F%6D", encodeAll("http://www.google.com"));
     }
 
 }
