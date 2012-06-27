9e1c2418a1b097758ee3f37ea815b0742f91a3e5
Christian Balaguer Jehle <christian.balaguer@namics.com>
2012-06-27 17:16:30 +0200

    minor code improvements

--- fizz-buzz/src/main/ch/crisi/tdd/percentEncoder/implementation/FizzBuzzImpl.java
+++ fizz-buzz/src/main/ch/crisi/tdd/percentEncoder/implementation/FizzBuzzImpl.java
@@ -1,13 +1,22 @@
 package ch.crisi.tdd.percentEncoder.implementation;
 
 import ch.crisi.tdd.percentEncoder.FizzBuzz;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class FizzBuzzImpl {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            System.out.print(FizzBuzz.process(i) + " ");
+        List<String> output = new ArrayList<String>();
+
+        for (int i = -100; i < 100; i++) {
+            output.add(FizzBuzz.process(i));
         }
+
+        System.out.print(StringUtils.join(output, ", "));
     }
     
 }
